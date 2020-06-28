9adf572c279b1971f74267884fd1722789e00757
Muhammad-Mustafa <mustafaraziq30@gmail.com>
2020-06-28 22:14:51 +0500

    Char.js Implemented

--- src/pages/Component/Result.js
+++ src/pages/Component/Result.js
@@ -1,39 +1,22 @@
-import React, { useState, useEffect } from "react";
-// import {getDailyData} from './../../api';
+import React, { useEffect, useState } from "react";
+
+import {
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarNav,
+  MDBNavItem,
+  MDBNavLink,
+  MDBNavbarToggler,
+  MDBCollapse
+} from "mdbreact";
+import { BrowserRouter as Router, useHistory } from "react-router-dom";
+import "./../../App.css";
+import * as firebase from "firebase";
 import { Line, Bar } from 'react-chartjs-2';
-import * as firebase from "firebase"
+
 
 export default function  Result(){
-return(
-    <div>
-        <div>
-            <h1>Result of your Constituency !</h1>
-        </div>
-    <div>
-    <Bar
-    data={{
-      labels: ['PTI', 'PMLN', 'PPP', 'Independent'],
-      datasets:[{
-        label:'Votes',
-        backgroundColor:[
-          'rgba(0,0,255,0.5)',
-          'rgba(0,255,0,0.5)',
-          'rgba(255,0,0,0.5)',
-          'rgba(255,0,0,0.8)',
-        ],
-        data:["5698", "4625", "3468", "2795"],
-      }]
-    }}
-    options={{
-      legend: {display: false},
-      title: {display: true, title: "Result"},
-    }}  
-    />
-</div>
-    </div>
-)
-}
-
+  let history = useHistory();
 
 const [election, setElection] = useState("deployed")
 let r = null;
@@ -48,6 +31,40 @@ useEffect(() => {
       setElection(snapshot.val().status);
   });
 },[])
+
+function Vote() {
+  history.push("/Voting");
+}
+function SignOut() {
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      history.push("/LoginVoter");
+    })
+    .catch(error => {
+      alert(error.message);
+    });
+}
+function Result(){
+  history.push("/Result");
+}
+
+function VoterPortal() {
+  history.push("/VoterPortal");
+}
+
+function CanListVoter() {
+  history.push("/CanListVoter" /*,{userId}*/);
+}
+
+function QrCodeGenerator(){
+
+  history.push({
+        pathname: "/QrCodeGenerator",
+        // state: userId,
+      });
+}
 
 const noResult  = (
   <h1>No Results to show</h1>
@@ -87,8 +104,57 @@ if(election == "deployed"){
 
 return(
   <div>
-    {r}
+    <div>
+        <Router>
+          <MDBNavbar className="navbar-voterProtal" dark expand="md">
+            <MDBNavbarBrand>
+              <strong className="white-text">Voter Portal</strong>
+            </MDBNavbarBrand>
+            <MDBNavbarToggler /*onClick={this.toggleCollapse}*/ />
+            <MDBCollapse
+              id="navbarCollapse3"
+              /*isOpen={this.state.isOpen}*/ navbar
+            >
+              <MDBNavbarNav className="navbar-voterPanal-navlist" left>
+                <MDBNavItem className="navItmes-voterProtal" active>
+                  <MDBNavLink to="" onClick={VoterPortal}>
+                    Dashboard
+                  </MDBNavLink>
+                </MDBNavItem>
 
+                <MDBNavItem className="navItmes-voterProtal">
+                  <MDBNavLink to="" onClick={CanListVoter}>
+                    Candidates list
+                  </MDBNavLink>
+                </MDBNavItem>
+                <MDBNavItem className="navItmes-voterProtal">
+                <MDBNavLink to="" onClick={Vote}>
+                  Vote
+                </MDBNavLink>
+              </MDBNavItem>
+              <MDBNavItem className="navItmes-voterProtal">
+                <MDBNavLink to="" onClick={QrCodeGenerator}>
+                  QR-Code
+                </MDBNavLink>
+              </MDBNavItem>
+              <MDBNavItem className="navItmes-voterProtal">
+                <MDBNavLink to="" onClick={Result}>
+                  Result
+                </MDBNavLink>
+              </MDBNavItem>
+              </MDBNavbarNav>
+              <MDBNavbarNav right>
+                <MDBNavItem className="navItmes-voterProtal">
+                  <MDBNavLink to="" onClick={SignOut}>
+                    SignOut
+                  </MDBNavLink>
+                </MDBNavItem>
+              </MDBNavbarNav>
+            </MDBCollapse>
+          </MDBNavbar>
+        </Router>
+      </div>
+    {r}
   </div>
 )
 }
